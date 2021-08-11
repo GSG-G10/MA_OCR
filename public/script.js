eba3ede3b3668115e5452f6b43f5d007eacb97c0
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-08-11 12:35:24 +0300

    flix something

---
+++ public/script.js
@@ -0,0 +1,10 @@
+// const changeFileUrl = document.querySelector('.type-select');
+// const changeUrl = document.querySelector('.url');
+// const changefill = document.querySelector('.file');
+
+// const swatch = changeUrl.value === 'url'
+
+
+// if (changeUrl.value === 'url'){
+
+// }
