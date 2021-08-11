745995414036ac6ceeb49956572d24839d45d870
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-08-11 13:44:42 +0300

    edit style

--- public/script.js
+++ public/script.js
@@ -1,12 +1,3 @@
-// const changeFileUrl = document.querySelector('.type-select');
-// const changeUrl = document.querySelector('.url');
-// const changefill = document.querySelector('.file');
-
-// const swatch = changeUrl.value === 'url'
-
-// if (changeUrl.value === 'url'){
-
-// }
 const ocrForm = document.querySelector('#ocr-form');
 const select = document.querySelector('#select');
 const ocrUrl = document.querySelector('#ocr-url');
