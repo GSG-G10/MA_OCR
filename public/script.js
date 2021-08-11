40618d10393f39f37ca6530c71a50ade12195eb4
AhemdAboAbadi <ahmed.free.py@gmail.com>
2021-08-11 18:48:25 +0300

    finsh style

--- public/script.js
+++ public/script.js
@@ -4,23 +4,28 @@ const select = document.querySelector('#select');
 const ocrUrl = document.querySelector('#ocr-url');
 const language = document.querySelector('#language');
 const textArea = document.querySelector('textarea');
+const imgContainer = document.querySelector('#ocr-img-container');
+const ocrImg = imgContainer.firstElementChild;
+const ocrFile = document.querySelector('#file');
+
+const imgRender = () => {
+  if (ocrFile.files && ocrFile.files[0]) {
+    console.log('hell');
+    ocrImg.src = URL.createObjectURL(ocrFile.files[0]);
+    ocrImg.style.display = 'block';
+  } else ocrImg.src = './img/defalut.png';
+};
 ocrForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (select.value == 'file') {
     fetch('/ocr/file', {
       body: new FormData(ocrForm),
       method: 'post',
-    })
-      .then((response) => response.json())
+    }).then((response) => response.json())
       .then((data) => {
         if (textArea.firstChild) textArea.removeChild(textArea.firstChild);
-        textArea.appendChild(
-          document.createTextNode(
-            data == '' ? "The img doesn't include any text..." : data
-          )
-        );
-      })
-      .catch((err) => console.log(err));
+        textArea.appendChild(document.createTextNode(data == '' ? 'The img doesn\'t include any text...' : data));
+      }).catch((err) => console.log(err));
   } else {
     fetch('/ocr/url', {
       headers: {
@@ -32,17 +37,11 @@ ocrForm.addEventListener('submit', (e) => {
         lang: language.value,
       }),
       method: 'post',
-    })
-      .then((response) => response.json())
+    }).then((response) => response.json())
       .then((data) => {
         if (textArea.firstChild) textArea.removeChild(textArea.firstChild);
-        textArea.appendChild(
-          document.createTextNode(
-            data == '' ? "The img doesn't include any text..." : data
-          )
-        );
-      })
-      .catch((err) => console.log(err));
+        textArea.appendChild(document.createTextNode(data == '' ? 'The img doesn\'t include any text...' : data));
+      }).catch((err) => console.log(err));
   }
 });
 
